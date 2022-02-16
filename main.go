39f6a597a4904b6ca5902e2d1f61cc7b0eafccbf
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-02-16 22:42:55 +0000

    created update recipe endpoint

--- main.go
+++ main.go
@@ -46,6 +46,37 @@ func ListRecipesHandler(c *gin.Context){
 	c.JSON(200, recipes)
 }
 
+// Update a recipe by id handler
+func UpdateRecipeHandler(c *gin.Context){
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++{
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Not Found",
+		})
+	}
+
+	recipes[index] = recipe
+
+	// TODO: More input checking is needed to be production ready
+	// TODO: Empty fields passed will remove values including id
+	// TODO: For now, it is good
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main(){
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -56,6 +87,7 @@ func main(){
 
 	r.POST("/recipes", NewRecipeHandler)
 	r.GET("/recipes", ListRecipesHandler)
+	r.PUT("/recipes/:id", UpdateRecipeHandler)
 
 	r.Run()
 }
